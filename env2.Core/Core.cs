6dd7aed18efe691ef9391ab4f99e0d62d5c58e56
Andrei Muski <magicmanam@gmail.com>
2020-10-06 20:27:33 +0300

    1) Adds methods to open DBs 2) Adds Demo page

--- env2.Core/Core.cs
+++ env2.Core/Core.cs
@@ -1,9 +1,40 @@
 ﻿using System.Text;
+using WebMatrix.Data;
 
 namespace env2.Core
 {
     public static class Core
     {
+        /// <summary>
+        /// Gets env2-service database.
+        /// </summary>
+        /// <returns></returns>
+        public static Database GetDB()
+        {
+            return Core.GetDB($"Server=localhost; Database=env2; Integrated Security=SSPI");
+        }
+
+        /// <summary>
+        /// Opens database with provided connection string.
+        /// </summary>
+        /// <param name="connectionString">Connection string.</param>
+        /// <returns></returns>
+        public static Database GetDB(string connectionString)
+        {
+            return Database.OpenConnectionString(connectionString, "System.Data.SqlClient");
+        }
+
+        /// <summary>
+        /// Opens database on the provided server.
+        /// </summary>
+        /// <param name="server">Server name.</param>
+        /// <param name="database">Database name.</param>
+        /// <returns></returns>
+        public static Database GetDB(string server, string database)
+        {
+            return Core.GetDB($"Server={server}; Database={database}; Integrated Security=SSPI");
+        }
+
         public static string Link(string environmentAlias, string linkWithoutQueryParameters, params string[] queryParameters)
         {
             var builder = new StringBuilder($"{linkWithoutQueryParameters}?");
