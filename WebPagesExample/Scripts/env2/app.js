988db14d7832cb69c5b9755cad337581f79294e5
Andrei Muski <magicmanam@gmail.com>
2020-11-25 09:57:15 +0300

    Adds tips module

--- WebPagesExample/Scripts/env2/app.js
+++ WebPagesExample/Scripts/env2/app.js
@@ -8,6 +8,33 @@ window.onscroll = function scrollFunction() {
     pro.id('2-top').style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? 'block' : 'none';
 };
 
+window.onbeforeunload = function (e) {
+    if (!window.isUnloading) {
+        let tip = tips[Math.floor(Math.random() * tips.length)];
+        let tipHtml = '<div style="display: table; width: 100%"><div style="height: 600px; display: table-cell; vertical-align: middle;"><h3></h3><div style="margin: 0 auto; width: 50%"><div style="font-weight: bold">'
+            + tip + '</div><!--<div><a>Next Tip</a>, Close</div>--></div></div></div>';
+        
+        sessionStorage.setItem('last-tip', JSON.stringify({ tip: tip, time: new Date().getTime() }));
+        //$('.env2-content-wrapper.main-content').html(tipHtml);
+        noty({ text: tip, layout: 'bottomCenter', type: 'information', timeout: 5500 });
+        
+        window.isUnloading = true;
+    }
+};
+
+window.onload = function (e) {
+    var lastTip = sessionStorage.getItem('last-tip');
+
+    if (lastTip) {
+        lastTip = JSON.parse(lastTip);
+        lastTip.time = 5555 - new Date().getTime() + lastTip.time;
+
+        if (lastTip.time > 0) {
+            noty({ text: lastTip.tip, layout: 'bottomCenter', type: 'information', timeout: lastTip.time });
+        }
+    }
+};
+
 function topFunction() {
     document.body.scrollTop = 0; // Safari
     document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
---
+++ WebPagesExample/Scripts/env2/common/modules/tips/_.js
@@ -0,0 +1,17 @@
+﻿var tips = [
+    // Common
+    'Unobtrusive tips are very efficient mnemotechnic tool!',
+
+    // HowTo list
+    'HOW TO: Be more productive? - Answer: Use Env2 tool on work',
+
+    // DoNot list
+    'Do not forget about exercises!',
+    //'',
+
+    // DidYouKnow
+    'Productive people are more happier',
+
+    // Ads
+    'Suggest your short and useful tip to Env2 portal administrator and get bonus points $)'
+];
